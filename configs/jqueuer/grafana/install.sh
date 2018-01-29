1a0baa93fc4753a471eae00efc90d701ff009e40
Osama <oabuoun1>
2018-01-29 09:50:41 +0000

    updating grafana/prometheus config

--- configs/jqueuer/grafana/install.sh
+++ configs/jqueuer/grafana/install.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-curl --header "Content-Type: application/json" --data @/etc/grafana/jqueuer/datasource.json http://admin:f4c85f99a0f97a5402e2c0faeefc2355e3df785b62f6254e89074b880a682b19@127.0.0.1:3000/api/datasources/
-curl --header "Content-Type: application/json" --data @/etc/grafana/jqueuer/dashboard.json http://admin:f4c85f99a0f97a5402e2c0faeefc2355e3df785b62f6254e89074b880a682b19@127.0.0.1:3000/api/dashboards/db
+curl --header "Content-Type: application/json" --data @/etc/grafana/jqueuer/datasource.json http://admin:f4c85f99a0@127.0.0.1:3000/api/datasources/
+curl --header "Content-Type: application/json" --data @/etc/grafana/jqueuer/dashboard.json http://admin:f4c85f99a0@127.0.0.1:3000/api/dashboards/db
