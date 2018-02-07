106a66357c159f7459d510513648d0966c7c57e1
Osama <oabuoun1>
2018-02-07 10:31:28 +0000

    update stats

--- configs/jqueuer/grafana/install.sh
+++ configs/jqueuer/grafana/install.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
-sleep 120
 curl --header "Content-Type: application/json" --data @/etc/grafana/jqueuer/datasource.json http://admin:f4c85f99a0@127.0.0.1:3000/api/datasources/
 curl --header "Content-Type: application/json" --data @/etc/grafana/jqueuer/dashboard.json http://admin:f4c85f99a0@127.0.0.1:3000/api/dashboards/db
